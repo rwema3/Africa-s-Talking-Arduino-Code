20218d37a0d44ae60fb0c326b5f190d0fab88b52
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-03-22 23:44:55 +0200

    flowLitres

--- waterFlow_sensor_using_esp32.ino
+++ waterFlow_sensor_using_esp32.ino
@@ -93,6 +93,7 @@ void loop()
  
     // Add the millilitres passed in this second to the cumulative total
     totalMilliLitres += flowMilliLitres;
+    totalLitres += flowLitres;
     availableLitter = currentLiter/1000;
     
     // Print the flow rate for this second in litres / minute
